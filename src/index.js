e9acf14163741135d99843aa3b7a3f1b8f3edf26
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-07-26 22:18:16 -0700

    add renderJoke function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ const BASE_URL = 'https://v2.jokeapi.dev/joke/programming'
 function fetchJoke(){
     fetch(BASE_URL)
     .then(res => res.json())
-    .then(console.log)
-    // .then(data => renderJoke(data))
+    // .then(console.log)
+    .then(data => renderJoke(data))
 }
 
+function renderJoke(data){
+    const jokeContainer = document.getElementById('joke-container')
+    const h3Tag = document.createElement('h3')
+    const h4Tag = document.createElement('h4')
 
+    if (data.type == 'single'){
+            h3Tag.innerText = data.joke
+            jokeContainer.appendChild(h3Tag)
+    } else {
+            h3Tag.innerText = data.setup
+            h4Tag.innerText = data.delivery
+            jokeContainer.appendChild(h3Tag)
+            jokeContainer.appendChild(h4Tag)
+
+
+    }
+}
 
 fetchJoke()
 
