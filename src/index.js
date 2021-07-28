9e95832e4a3445df6a1bd4635ad31ad40a007636
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-07-28 11:11:05 -0700

    fix to clear before render joke

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const BASE_URL = 'https://v2.jokeapi.dev/joke/programming'
 
+const jokeContainer = document.getElementById('joke-container')
+
+const h3Tag = document.createElement('h3')
+const h4Tag = document.createElement('h4')
+
 function fetchJoke(){
     fetch(BASE_URL)
     .then(res => res.json())
-    // .then(console.log)
     .then(data => renderJoke(data))
 }
 
-function renderJoke(data){
-    const jokeContainer = document.getElementById('joke-container')
-    const h3Tag = document.createElement('h3')
-    const h4Tag = document.createElement('h4')
-
+function renderJoke(data){    
     if (data.flags.values != true && data.id != 47 && data.id != 8) {
+
         if (data.type == 'single'){
             h3Tag.innerText = data.joke
             jokeContainer.appendChild(h3Tag)
@@ -25,6 +26,17 @@ function renderJoke(data){
     }
 }
 
-fetchJoke()
+function getJoke(){
+    const jokeButton = document.getElementById('button')
+
+    jokeButton.addEventListener("click", () => {
+        h3Tag.innerText = ""
+        h4Tag.innerText = ""
+        fetchJoke()
+    })
+}
+
+getJoke()
+
 
 
