7313d7b7f83d6a02578c922d5467c89535b8f1e6
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-07-28 12:59:20 -0700

    add pun functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const PROG_URL = 'https://v2.jokeapi.dev/joke/programming'
 const PUN_URL = 'https://v2.jokeapi.dev/joke/pun'
 
-
 const jokeContainer = document.getElementById('joke-container')
 
 const h3Tag = document.createElement('h3')
 const h4Tag = document.createElement('h4')
 
-function fetchJoke(){
+function fetchJokeProg(){
     fetch(PROG_URL)
     .then(res => res.json())
-    .then(data => renderJoke(data))
+    .then(data => renderJokeProg(data))
 }
 
-function renderJoke(data){    
+function renderJokeProg(data){    
     if (data.flags.values != true && data.id != 47 && data.id != 8) {
 
         if (data.type == 'single'){
@@ -28,17 +27,50 @@ function renderJoke(data){
     }
 }
 
-function getJoke(){
+function getJokeProg(){
     const jokeButton = document.getElementById('button1')
 
     jokeButton.addEventListener("click", () => {
         h3Tag.innerText = ""
         h4Tag.innerText = ""
-        fetchJoke()
+        fetchJokeProg()
+    })
+}
+// ********
+
+function fetchJokePun(){
+    fetch(PUN_URL)
+    .then(res => res.json())
+    .then(data => renderJokePun(data))
+}
+
+function renderJokePun(data){    
+    if (data.flags.values != true) {
+
+        if (data.type == 'single'){
+            h3Tag.innerText = data.joke
+            jokeContainer.appendChild(h3Tag)
+        } else {
+            h3Tag.innerText = data.setup
+            h4Tag.innerText = data.delivery
+            jokeContainer.appendChild(h3Tag)
+            jokeContainer.appendChild(h4Tag)
+        }
+    }
+}
+
+function getJokePun(){
+    const jokeButton = document.getElementById('button2')
+
+    jokeButton.addEventListener("click", () => {
+        h3Tag.innerText = ""
+        h4Tag.innerText = ""
+        fetchJokePun()
     })
 }
 
-getJoke()
+getJokeProg()
+getJokePun()
 
 
 
